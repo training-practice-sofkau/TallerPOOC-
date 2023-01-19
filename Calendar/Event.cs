9a121cde8aefdd9186794f89c63b96ef4d6188a3
Adryan Ynfante  Valero <adryan.ynfante@sofka.local>
2023-01-19 11:36:26 -0500

    POO en C# taller-mclase event para iniciar taller calendario

---
+++ Calendar/Event.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    internal class Event
+    {
+        private string _name;
+        private DateTime _startDate;
+        private DateTime _endDate;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+            set { _startDate = value; }
+        }
+
+        public DateTime EndDate
+        {
+            get { return _endDate; }
+            set { _endDate = value; }
+        }
+
+        public Event(string name, DateTime startDate, DateTime endDate)
+        {
+            Name = name;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
