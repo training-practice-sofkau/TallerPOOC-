4d9ea76e2606bc7ef93ce0a17229465938f5891a
Kevin Baquero <baquerochavarro@gmail.com>
2023-01-20 18:21:14 -0500

    Adding Calendary class

---
+++ Calendar/Calendary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    internal class Calendary
+    {
+        private Day daysMonth;
+
+        public Day DaysMonth
+        {
+            get { return daysMonth; }
+            set { daysMonth = value; }
+        }
+
+        public Calendary(Day daysMonth)
+        {
+            DaysMonth = daysMonth;
+        }
+    }
+}
