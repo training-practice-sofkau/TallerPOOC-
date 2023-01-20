1de3a61d206062eed13c06ec4d987ee71f6047f0
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-01-20 13:20:10 -0500

    Creacion Clase
    Se hizo la creacion de la clase day

---
+++ Calendar/Day.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace Calendar
+{
+    internal class Day
+    {
+
+        private string nameDay;
+        private int dayNumber;
+        private Event listEvent;
+
+      
+        public string Name {
+
+            get { return nameDay; }
+            set { nameDay = value; }
+        
+        }
+        public int DayNumber {
+
+            get { return dayNumber; }
+
+            set { dayNumber  = value; }
+        
+        }
+
+        public Event ListEvent {
+
+            get { return listEvent; }
+
+            set { listEvent = value; }
+        
+        }
+
+        public Day(int dayNumber, Event listEvent, string name)
+        {
+
+            Name = name;
+            DayNumber = dayNumber;
+            ListEvent = listEvent;
+        }
+
+    }
+}
