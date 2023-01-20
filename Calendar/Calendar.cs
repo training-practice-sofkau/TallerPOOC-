4c0b190988476a8cbee0956f9882d193d8d9db5a
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-01-20 15:07:16 -0500

    Creacion de clase
    Se realizo la creacion de la clase calendar

---
+++ Calendar/Calendar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Calendar
+{
+    internal class Calendar
+    {
+
+        private string month;
+        private int dayMonth;
+        private List<Day> listDay;
+
+
+        public string Month
+        {
+            get { return month; }
+            set { month = value; }
+        }
+        public int DayMonth
+        {
+            get { return dayMonth; }
+            set { dayMonth = value; }
+        }
+
+        public List<Day> ListDay
+        {
+            get { return listDay; }
+            set { listDay = value; }
+        }
+
+
+        public Calendar(string month,int dayMonth, List<Day>  listDay)
+        {
+            Month= month;
+            DayMonth= dayMonth;
+            ListDay = listDay;
+        }
+    
+    
+    
+    }
+}
