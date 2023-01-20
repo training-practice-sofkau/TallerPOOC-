ae237c25ed8eed9d02444d8d1796a9ad745439c2
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-01-20 15:34:48 -0500

    Modificacion
    Creacion del metodo main

--- Calendar/Program.cs
+++ Calendar/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Calendar {
+
+    internal class Program {
+
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Hello Word");
+
+
+        }
+
+    }
+
+}
+
+
+
+
