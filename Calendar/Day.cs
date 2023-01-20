ec1c2f8c9e52d479b807a6ea72122c62237cdffa
Kevin Baquero <baquerochavarro@gmail.com>
2023-01-20 18:08:21 -0500

    Adding Day class

---
+++ Calendar/Day.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    internal class Day
+    {
+        private List<Event> eventsDay;
+
+        public List<Event> EventsDay
+        {
+            get { return eventsDay; }
+            set { eventsDay = value; }
+        }
+
+        public Day(List<Event> eventsDay)
+        {
+            EventsDay = eventsDay;
+        }
+    }
+}
