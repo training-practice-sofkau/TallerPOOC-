bd62229af2646277c2d5fcbd3661b8c8c0d45116
Jhoan Gaviria <jmgaviriam@gmail.com>
2023-01-22 11:21:58 -0500

    feat: Adding, removing, and listing events to instances of the Calendar class

--- Calendar/Program.cs
+++ Calendar/Program.cs
@@ -10,7 +10,25 @@ namespace Calendar
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calendar calendar = new Calendar();
+
+            Event event1 = new Event("Event1", new DateTime(2015, 1, 1), new DateTime(2015, 1, 1));
+            Event event2 = new Event("Event2", new DateTime(2015, 1, 1), new DateTime(2015, 1, 1));
+            Event event3 = new Event("Event3", new DateTime(2015, 1, 2), new DateTime(2015, 1, 2));
+
+            calendar.AddEvent(event1);
+            calendar.AddEvent(event2);
+            calendar.AddEvent(event3);
+
+            Console.WriteLine("Events: ");
+            calendar.PrintEvents();
+
+            calendar.RemoveEvent(event2);
+
+            Console.WriteLine("Events: ");
+            calendar.PrintEvents();
+
+            Console.ReadKey();
         }
     }
 }
