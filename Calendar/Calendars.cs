b4310f2d1da4ec79496b63521969e5334f0d6221
Javier Carrillo <javiercamur98@gmail.com>
2023-01-23 01:37:11 -0500

    Se configura la clase main y se crean las instancias

--- Calendar/Calendars.cs
+++ Calendar/Calendars.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Calendar
 {
-    internal class Calendar
+    internal class Calendars
     {
         private string _descriptionCalendar;
         private Day[] _days = new Day[30];
 
-        public Calendar(string descriptionCalendar, Day[] days)
+        public Calendars(string descriptionCalendar, Day[] days)
         {
             _descriptionCalendar = descriptionCalendar;
             Days = days;
@@ -19,5 +19,10 @@ namespace Calendar
 
         public string DescriptionCalendar { get => _descriptionCalendar; set => _descriptionCalendar = value; }
         internal Day[] Days { get => _days; set => _days = value; }
+
+        public override string ToString()
+        {
+            return _descriptionCalendar +"\n"+ _days[0].NumberDay + "\n" + _days[1].NumberDay;
+        }
     }
 }
--- Calendar/Program.cs
+++ Calendar/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Calendar;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Event evento1 = new Event("Master class", new DateTime(2023, 01, 23, 8, 30, 00), new DateTime(2023, 01, 23, 9, 30, 00));
+        Event evento2 = new Event("Workshop .Net", new DateTime(2023, 01, 23, 11, 15, 00), new DateTime(2023, 01, 23, 12, 00, 00));
+        Event evento3 = new Event("Master class", new DateTime(2023, 01, 24, 8, 30, 00), new DateTime(2023, 01, 24, 9, 30, 00));
+        Event evento4 = new Event("Workshop .Net", new DateTime(2023, 01, 23, 8, 30, 00), new DateTime(2023, 01, 24, 12, 00, 00));
+
+        Day day1 = new Day(23, new[] {evento1, evento2});
+        Day day2 = new Day(24, new[] {evento3, evento4});
+
+        Calendars calendar1 = new Calendars("Ciclo1 .Net", new[] {day1, day2});
+
+        Console.WriteLine(calendar1.ToString());
+    }
+}
