1afdea3cf71c89cafa4ee7ab458ef46ae66f9081
Javier Carrillo <javiercamur98@gmail.com>
2023-01-23 00:53:32 -0500

    Se crea la clase Calendar

---
+++ Calendar/Calendar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    internal class Calendar
+    {
+        private string _descriptionCalendar;
+        private Day[] _days = new Day[30];
+
+        public Calendar(string descriptionCalendar, Day[] days)
+        {
+            _descriptionCalendar = descriptionCalendar;
+            Days = days;
+        }
+
+        public string DescriptionCalendar { get => _descriptionCalendar; set => _descriptionCalendar = value; }
+        internal Day[] Days { get => _days; set => _days = value; }
+    }
+}
